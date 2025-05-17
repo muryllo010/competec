0c7e4bf81a899f2f7a6d41562efca7fddc19a47a
muryllo010 <muryleco123@gmail.com>
2025-05-17 10:41:26 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
+
+
+
+
+
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+	    
+ char caractere = 'm';
+	   int idade = 15;
+	    boolean tomeicoffe = true;
+	    
+	    Scanner inicio = new Scanner (System.in);
+	    
+	    
+	    
+	    System.out.print("digite o valor da laranja");
+int laranja= inicio.nextInt();
+
+System.out.print("digite o valor da uva");
+int uva= inicio.nextInt();
+	    
+	    
+	    
+	    
+	    
+	   
+	    
+	   System.out.println("resultado da soma = "+(laranja + uva) );
+	    System.out.println("muryllo lindo");
 		System.out.println("Hello World");
 	}
 }
+
