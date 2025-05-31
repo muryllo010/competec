a279080f8170d222bece5741aee9611efde8983a
muryllo010 <muryleco123@gmail.com>
2025-05-31 11:08:14 -0300

    Update Main.java

--- aula-5/Main.java
+++ aula-5/Main.java
@@ -1,7 +1,35 @@
 public class Main
 {
+	public static void main(String[] args) {import java.util.Scanner;
+
+public class Main
+{
+
+ 
+
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+	
+ Scanner entrada = new Scanner(System.in);
+ 
+ 
+   int tamanho = entrada.nextInt();
+   int valores[] = new int [tamanho]; 
+
+  for(int i = 0; i < valores.length;i++){
+ System.out.println("digite o numero inteiro");
+     valores[i] = entrada.nextInt();
+     
+
+  }
+    for(int i = 0; i < valores.length;i++){
+ System.out.println(valores[i]);
+
+     
+
+  }
+ 
+}
+}
 		System.out.println("Hello World");
 	}
 }
