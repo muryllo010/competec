b38d025fec343005ad393e1d2ff8fb7bb4aff891
muryllo010 <muryleco123@gmail.com>
2025-06-28 11:10:05 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -1,7 +1,23 @@
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+          
+    System.out.println("Qual a palavra SECRETA");
+     String Secreta = "  BATATA"; 
+     String palavra = digite.nextline();
+    
+     
+     boolean resultado = (entrada.equals.IgnoreCase(secreta));   
+    if(resultado == true);{
+    }else    {
+    System.out.println("parabens, voce acertou!");
+    System.out.println("Que pena, voce errou!");
+
+
+    }
+	} 
+    
+    
 		System.out.println("Hello World");
 	}
 }
